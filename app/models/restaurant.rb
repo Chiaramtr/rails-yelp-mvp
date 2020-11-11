c96019de63bd799d096561186dd82dbaa193e2d5
Chiara de Montlaur <chiarademontlaur@gmail.com>
2020-11-11 16:36:26 +0100

    Yelp MVP

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,8 @@
 class Restaurant < ApplicationRecord
+  TYPES = %w(chinese italian japanese french belgian)
+  has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :category, inclusion: { in: TYPES }
+  validates :address, presence: true
+  validates :phone_number, presence: true
 end
---
+++ db/migrate/20201111151055_delete_rating_to_restaurants.rb
@@ -0,0 +1,5 @@
+class DeleteRatingToRestaurants < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :restaurants, :rating, :integer
+  end
+end
